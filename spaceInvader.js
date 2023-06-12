6ea20871ab4b58bf60dd825e568423617c6d8a36
Sam Hoeterickx <sam.hoeterickx@student.ehb.be>
2023-06-12 10:13:53 +0200

    spaceInvaders_Sam_Hoeterickx

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,14 +1,40 @@
 "use strict";
 import * as Utils from "./scripts/utils.js";
 import context from "./scripts/context.js";
-
-alert("Draw your space invader here");
+/* 
+alert("Draw your space invader here"); */
 
 drawBackground();
-
+drawAvatar();
 function drawBackground(){
     context.fillStyle = "Black";
     context.beginPath();
     context.rect(0, 0, 300, 300);
     context.fill();
+}
+
+function drawAvatar(){
+    context.fillStyle = "#66ff00";
+    
+    context.fillRect(25, 75, 50, 50);
+    context.fillRect(75, 25, 50, 50);
+    context.fillRect(225, 75, 50, 50);
+    context.fillRect(175, 25, 50, 50);
+    
+    context.fillRect(125, 125, 50, 50);
+    context.fillRect(75, 125, 50, 50);
+    context.fillRect(175, 125, 50, 50);
+    context.fillRect(125, 175, 50, 50);
+    context.fillRect(75, 175, 50, 50);
+    context.fillRect(175, 175, 50, 50);
+
+    context.fillRect(25, 175, 50, 50);
+    context.fillRect(25, 225, 50, 50);
+    context.fillRect(75, 175, 50, 50);
+    context.fillRect(75, 225, 50, 50);
+    
+    context.fillRect(225, 175, 50, 50);
+    context.fillRect(225, 225, 50, 50);
+    context.fillRect(175, 175, 50, 50);
+    context.fillRect(175, 225, 50, 50);
 }
