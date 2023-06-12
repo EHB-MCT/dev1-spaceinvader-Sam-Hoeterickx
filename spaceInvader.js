baf758e726124cf7f4cef7bef06e414839ac8542
Sam Hoeterickx <sam.hoeterickx@student.ehb.be>
2023-06-12 10:15:50 +0200

    spaceInvaders_Sam_Hoeterickx-final

--- spaceInvader.js
+++ spaceInvader.js
@@ -15,24 +15,23 @@ function drawBackground(){
 
 function drawAvatar(){
     context.fillStyle = "#66ff00";
-    
+    //links boven
     context.fillRect(25, 75, 50, 50);
     context.fillRect(75, 25, 50, 50);
+    //rechts boven
     context.fillRect(225, 75, 50, 50);
     context.fillRect(175, 25, 50, 50);
-    
+    //midden
     context.fillRect(125, 125, 50, 50);
     context.fillRect(75, 125, 50, 50);
     context.fillRect(175, 125, 50, 50);
     context.fillRect(125, 175, 50, 50);
-    context.fillRect(75, 175, 50, 50);
-    context.fillRect(175, 175, 50, 50);
-
+    //links onder
     context.fillRect(25, 175, 50, 50);
     context.fillRect(25, 225, 50, 50);
     context.fillRect(75, 175, 50, 50);
     context.fillRect(75, 225, 50, 50);
-    
+    //Rechts onder
     context.fillRect(225, 175, 50, 50);
     context.fillRect(225, 225, 50, 50);
     context.fillRect(175, 175, 50, 50);
